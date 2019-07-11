309a14c7abd72be06a1bd44d640813c012f63d3e
dylanosaur <dadams10642@outlook.com>
2019-07-11 10:49:10 -0400

    github edit

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use('/reimbursements', reimbursementsRouter)
 // use delete property where posssible instead of stripping reimbursementID
 // make db calls more explicit ("be explicit, not implicit" -Zen of Python)
 // re make db so that model field keys and db field keys are 1 to 1
-//
+// test change
 // stretch to do
 // hash passwords and compare hashes
 // add QC on inputs
