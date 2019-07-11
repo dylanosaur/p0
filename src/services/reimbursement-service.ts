96c9a14a56910cb64fb0b2edf8b6573962e5f439
Dylan Adams <dadams10642@outlook.com>
2019-07-10 20:34:57 -0400

    show t ojaye

--- src/services/reimbursement-service.ts
+++ src/services/reimbursement-service.ts
@@ -20,24 +20,35 @@ export function getReimbursementsFromStatus(status) {
     return result
 }
 
-export function addReimbursement(userId, body): Reimbursement {
+export async function addReimbursement(userId, body) {
     let newRId: Number = refunds.length + 1
     // should probably onlyl require certain fields and should check for those fields
-    let newReimbursement = new Reimbursement(1, userId, 0, String(Date()), '', '', 1, 0, 4);
-    for (let key of Object.keys(body)) { 
-        newReimbursement[key] = body[key]
-    }
-    newReimbursement['reimbursementId'] = newRId;
-    db.refunds.push(newReimbursement);
-    return newReimbursement
+    let newReimbursement = new Reimbursement(7, userId, 0, 0, 0, ' ', 1, 1, 4);
+    let v = newReimbursement;
+    let k = Object.keys(newReimbursement);
+    let valuesString = ` ${v[k[0]]}, ${v[k[1]]}, ${v[k[2]]}, ${v[k[3]]}, '${v[k[4]]}', 
+                        '${v[k[5]]}', ${v[k[6]]}, ${v[k[7]]}, ${v[k[8]]} `
+    let updateString = `Insert into reimbursements values (${valuesString});`
+    console.log(updateString);
+    await qDB.queryDB(updateString);
+    let queryString = `select * from reimbursements 
+                WHERE reimbursementid = ${v[k[0]]};`
+    console.log(queryString);
+    let result = await qDB.queryDB(queryString);
+    return result
 }
 
-export function updateReimbursement(body): Reimbursement|null {
-    let matchRefundsWithRefundId = (refund) => (refund.reimbursementId=== parseInt(body['reimbursementId']));
-    let matchedRefund: Reimbursement = refunds.filter(matchRefundsWithRefundId)[0];
-    if (!(matchedRefund)) return null
-    for (let key of Object.keys(body)) { 
-        matchedRefund[key] = body[key]
-    }
-    return matchedRefund
+export async function updateReimbursement(body) {
+    let keysValuesString = ''
+    for (let key of Object.keys(body)) { keysValuesString += ` ${key} = ${body[key]}, `}
+    keysValuesString = keysValuesString.substring(0, keysValuesString.lastIndexOf(',')) //remove the final comma
+    let updateString = `UPDATE reimbursements SET ${keysValuesString} 
+                WHERE reimbursementid = ${parseInt(body['reimbursementId'])};`
+    console.log(updateString);
+    await qDB.queryDB(updateString);
+    let queryString = `select * from reimbursements 
+                WHERE reimbursementid = ${parseInt(body['reimbursementId'])};`
+    console.log(queryString);
+    let result = await qDB.queryDB(queryString);
+    return result
 }
