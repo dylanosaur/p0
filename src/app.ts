6b2b4d6845dee7856b31f640b3d6ac9279902aff
Dylan Adams <dadams10642@outlook.com>
2019-07-13 19:34:43 -0400

    added lines to app.ts to close port as part of shutdown process

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,28 @@ import cookieParser = require("cookie-parser");
 import usersRouter from './routers/users-router'
 import loginRouter from './routers/login-router'
 import reimbursementsRouter from './routers/reimbursements-router'
+import db from './sql-service/pg-connect'
 
 const app = express();
 
 //Enable express to use body-parser and cookie-parser as middle-ware
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(express.json());
 
 // define socket port channel for app/server communication and start listening
-const port = 3000
-app.listen(port, () => console.log(`ERS app listening on port ${port}!`))
+const port = 3000;
+const port_handler = app.listen(port, () => console.log(`ERS app listening on port ${port}!`));
 
 app.use('/login', loginRouter);
 app.use('/users', usersRouter);
 app.use('/reimbursements', reimbursementsRouter)
 
+// Close the pool when app shuts down
+process.on('SIGINT', () => {
+    db.end().then(() => console.log('pool has ended'));
+    port_handler.close();
+});
 
 // todo
 // test all the way through at least once more
