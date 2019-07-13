4b53f4d3e59e31e8bbb1529991179293753a3ca8
Dylan Adams <dadams10642@outlook.com>
2019-07-13 02:10:34 -0400

    added the rest of the db querying, added hashing for security

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -1,22 +1,54 @@
 import User from '../models/User'
-import db from '../models/db'
-let users = db.users;
+import utilities from './utilities'
+import db from '../sql-service/pg-connect'
 
-// similiar filter operation to matchUserAndPassword but with userId
-export function matchUserWithUserId(userId): User {
-    let matchUserWithId = (user) => (userId === String(user.userId))
-    let matchedUser: User = users.filter(matchUserWithId)[0];
+export async function matchUserWithUserId(userId): Promise<User> {
+    const queryString = `select * from users where userid = $1`;
+    const userResults = await db.query(queryString, [userId]);
+    const userData = userResults.rows[0];
+    const matchedUser = new User();
+    // read in all of the properties on a single user to User typed objected
+    for (let key of Object.keys(matchedUser)) { matchedUser[key] = userData[key.toLowerCase()]; }
     return matchedUser
 }
 
-export function updateUser(user, body): User {
-    for (let key of Object.keys(body)) { 
-        user[key] = body[key]
+export async function updateUser(userId, body): Promise<User> {
+    const nullUser = new User();
+    const updateUser = {}
+    // loop over keys in User object for possible update-able keys
+    for (let key of Object.keys(body)) {
+        // if one of those keys is present in the body of the request, read it into updateUser
+        // this is sanitization / QC on the information present in the request body
+        if (Object.keys(nullUser).includes(key)) { 
+            updateUser[key] = body[key]; 
+        }
     }
-    return user
+    const nKeys = Object.keys(updateUser).length;
+    console.log(nKeys, Object.keys(updateUser));
+    const formatString = utilities.moneyString(1,nKeys+1);
+    const updateString = `update users set (${Object.keys(updateUser).join(', ')}) 
+                        = (${formatString}) where userid = $${nKeys+1} returning *;`;
+    console.log(updateString)
+    const userResults = await db.query(updateString, userId);
+    const userData = userResults.rows[0];
+    const updatedUser = new User();
+    // read in all of the properties on a single user to User typed objected
+    for (let key of Object.keys(updatedUser)) { updatedUser[key] = userData[key.toLowerCase()]; }
+    return updatedUser
 }
 
-export function getAllUsers(): Array<User> {
+export async function getAllUsers(): Promise<Array<User>> {
+    const queryString = `select * from users`;
+    const usersResults = await db.query(queryString);
+    const usersData = usersResults.rows;
+    // read data into users object, user by user
+    const users = [];
+    for (let user of usersData){
+        users.push(new User());
+        let currentUser = users[users.length-1]
+        // read in all of the properties on a single user to User typed objected
+        for (let key of Object.keys(currentUser)) { currentUser[key] = user[key.toLowerCase()]; }
+    }
     return users;
 }
 
