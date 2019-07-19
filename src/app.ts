0f69dd7f242be3b5ee4a7d522e6834295a6df5c6
Dylan Adams <dadams10642@outlook.com>
2019-07-19 16:56:18 -0400

    jenkins upd test

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
- 
+  
     next();
   });
 
