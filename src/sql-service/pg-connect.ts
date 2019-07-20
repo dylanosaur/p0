e4322b7cfc4cfc77a32619a0e95375444380f034
Dylan Adams <dadams10642@outlook.com>
2019-07-20 19:49:53 -0400

    changed credentials for db

--- src/sql-service/pg-connect.ts
+++ src/sql-service/pg-connect.ts
@@ -1,11 +1,12 @@
+
 const Pool = require('pg').Pool
 
 // TODO - move pool to app.ts level so one pool is used repeatedly
 const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'p0-ERS',
-  password: 'password',
+  user: process.env.POSTGRES_USN || 'postgres',
+  host: process.env.POSTGRES_URL || 'localhost',
+  database: 'database-1',
+  password: process.env.POSTGRES_PASS|| 'password',
   port: 5432,
 });
 
