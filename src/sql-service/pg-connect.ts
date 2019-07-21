10dbe08f9ebcf6fcafeaa9de31fd1cd795eb2e88
Dylan Adams <dadams10642@outlook.com>
2019-07-20 20:36:13 -0400

    fixing db credentials

--- src/sql-service/pg-connect.ts
+++ src/sql-service/pg-connect.ts
@@ -5,7 +5,7 @@ const Pool = require('pg').Pool
 const db = new Pool({
   user: process.env.POSTGRES_USN || 'postgres',
   host: process.env.POSTGRES_URL || 'localhost',
-  database: 'database-1',
+  database: 'postgres',
   password: process.env.POSTGRES_PASS|| 'password',
   port: 5432,
 });
