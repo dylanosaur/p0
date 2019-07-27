65f3dc262d04bdad2083a457c5787a28848214fb
Dylan Adams <dadams10642@outlook.com>
2019-07-26 22:47:27 -0400

    meh

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,8 @@ app.use(function (req, res, next) {
   });
 */
 app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', String(req.headers.origin));
+    //res.header('Access-Control-Allow-Origin', String(req.headers.origin));
+    res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", 'true');
     res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH');
