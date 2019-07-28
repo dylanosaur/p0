dde90b3733dadac58fd6c4befd8bb38aba93590b
Dylan Adams <dadams10642@outlook.com>
2019-07-28 11:00:54 -0400

    meh

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 //app.use(cors());
 
 app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //    res.header('Access-Control-Allow-Origin', String(req.headers.origin));
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", 'true');
