0341b1437ef87d76083f73ae630169538d097d78
Dylan Adams <dadams10642@outlook.com>
2019-07-28 11:22:38 -0400

    meh

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,11 @@ app.use(function (req, res, next) {
   });
 */
 //app.use(cors());
-
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+/*
 app.use(function(req, res, next) {  
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //    res.header('Access-Control-Allow-Origin', String(req.headers.origin));
@@ -39,7 +43,7 @@ app.use(function(req, res, next) {
 
     next();
 });
-
+*/
 // define socket port channel for app/server communication and start listening
 const port = 3000;
 const port_handler = app.listen(port, () => console.log(`ERS app listening on port ${port}!`));
